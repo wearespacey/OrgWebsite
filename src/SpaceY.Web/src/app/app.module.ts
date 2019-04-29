ece8f2255e2079ad9c7d3f71b3a359055907e125
Tinaël <tinael.devresse@mic-belgique.be>
2019-04-30 00:56:00 +0200

    Change files base folder

--- src/SpaceY.Web/src/app/app.module.ts
+++ src/SpaceY.Web/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
   MatButtonModule,
   MatCheckboxModule,
   MatGridListModule
 } from '@angular/material';
 import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
