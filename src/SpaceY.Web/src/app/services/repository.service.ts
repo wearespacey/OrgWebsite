e7686ef0fde4e1b29d740d142b3d734219eb3b60
Tinaël <tinael.devresse@mic-belgique.be>
2019-05-13 11:56:05 +0200

    Add core of the service to recover repos from organization

--- src/SpaceY.Web/src/app/services/repository.service.ts
+++ src/SpaceY.Web/src/app/services/repository.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Repository } from '../models/Models';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepositoryService {
+  private readonly baseUrl = 'https://api.github.com';
+  private readonly endpoint = '/orgs/wearespacey';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  public get(currentPage = 1): Observable<Repository[]> {
+    return this.httpClient.get<Repository[]>(`${this.baseUrl}${this.endpoint}/repos?page=${currentPage}`);
+  }
 }
