1ea0c0413a892362ba18ebbc794f8e67b769c1ab
Nathan Pire <nathan.pire@gmail.com>
2019-09-01 22:36:21 +0200

    edit contact us and menu

--- src/SpaceY.Web/src/app/components/contact/contact.component.ts
+++ src/SpaceY.Web/src/app/components/contact/contact.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
+  public name: string;
+  public subject: string;
   constructor() { }
 
   ngOnInit() {
   }
-
+  sendMail() {
+    window.open('mailto:nathan@mic-belgique.be?subject=SpaceY contact ' + this.name + '&body=' + this.subject);
+  }
 }
